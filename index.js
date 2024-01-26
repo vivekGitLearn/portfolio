28954e13777eaa54ce8e4d166222a75e161e01b8
Vivek Kumar <vivekkumarbeg7@gmail.com>
2024-01-26 19:48:00 +0530

    add index.js
    animation

---
+++ index.js
@@ -0,0 +1,19 @@
+var mySkills = ["Vivek Kumar", "Web Developer", "IoT Developer"];
+
+// Access the div with id "intro"
+let introDiv = document.getElementById("intro");
+
+// Function to display text after a delay
+function displayAfterDelay(text, delay) {
+    setTimeout(function () {
+        document.getElementById("intro").innerHTML = text;
+    }, delay);
+}
+
+// Call the function for each skill with a delay
+for (var k = 0; k < 100; k++){
+    for (var j = 0; j < mySkills.length; j++) {
+        // Multiply delay by (j + 1) to stagger the display
+        displayAfterDelay(mySkills[j], 1000 * (j + 1));
+    }
+}
