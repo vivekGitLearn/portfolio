9f631c2d350a8da2c05db297fcb18e469aa243d0
Vivek Kumar <vivekkumarbeg7@gmail.com>
2024-02-09 14:00:18 +0530

    add more android project

--- index.js
+++ index.js
@@ -18,16 +18,38 @@ for (var k = 0; k < 100; k++){
 }
 
 var androidP1=["img/AP_14.jpg","img/AP_11.jpg","img/AP_12.jpg","img/AP_13.jpg",];
+var androidP2=["img/AP_21.jpg","img/AP_22.jpg"];
+var androidP3=["img/AP_31.jpg","img/AP_32.jpg"];
 var ap1_index=0;
+var ap2_index=0;
+var ap3_index=0;
 
 function imgNext(id) {
     const img = document.getElementById(id);
-    ap1_index = (ap1_index + 1) % androidP1.length; // Calculate the next index
-    img.src = androidP1[ap1_index]; // Update the source to the next image
+    if (id === 'AP1') {
+        ap1_index = (ap1_index + 1) % androidP1.length;
+        img.src = androidP1[ap1_index];
+    } else if (id === 'AP2') {
+        ap2_index = (ap2_index + 1) % androidP2.length;
+        img.src = androidP2[ap2_index];
+    }
+    else if (id === 'AP3') {
+        ap3_index = (ap3_index + 1) % androidP3.length;
+        img.src = androidP3[ap3_index];
+    }
 }
 
 function imgPrev(id) {
     const img = document.getElementById(id);
-    ap1_index = (ap1_index - 1 + androidP1.length) % androidP1.length; // Calculate the previous index
-    img.src = androidP1[ap1_index]; // Update the source to the previous image
+    if (id === 'AP1') {
+        ap1_index = (ap1_index - 1 + androidP1.length) % androidP1.length;
+        img.src = androidP1[ap1_index];
+    } else if (id === 'AP2') {
+        ap2_index = (ap2_index - 1 + androidP2.length) % androidP2.length;
+        img.src = androidP2[ap2_index];
+    }
+    else if (id === 'AP3') {
+        ap3_index = (ap3_index - 1 + androidP3.length) % androidP3.length;
+        img.src = androidP3[ap3_index];
+    }
 }
